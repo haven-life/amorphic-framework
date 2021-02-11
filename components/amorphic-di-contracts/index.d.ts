e2a80ad3eff9f522fb40a1f84dbb4328f76fd1e3
Nick Saenz <nick@havenlife.com>
2021-02-11 15:15:49 -0500

    using index.d.ts syntax to follow types repo standards and also adding the remaining dependency injection module, the statsd library while we're here

---
+++ components/amorphic-di-contracts/index.d.ts
@@ -0,0 +1,22 @@
+/**
+ * application configuration store object
+ */
+export interface Config {
+    get(key: string): any
+    set(key: string, value: any): any
+    loadFile(fileKey: string, file: string): any
+    buildConfig(configRootDirectory: string, merge: boolean): any
+}
+
+/**
+ * monitoring module
+ */
+export interface StatsdClientInterface {
+    timing(statsKey: string, timer: number | Date, tags?: object): void
+    counter(statsKey: string, num: number, tags?: object): void
+}
+
+/**
+ * logger function
+ */
+export type LoggerFunction = (logLevel: string, logObject: any, ...rawLogData) => void
