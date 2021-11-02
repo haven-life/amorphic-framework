042521359bf935aa94f477054c1c55e1c04657b1
Shanks <shanks@havenlife.com>
2021-11-02 13:32:35 -0400

    adding bad indentation back

--- components/amorphic/lib/routes/processPost.js
+++ components/amorphic/lib/routes/processPost.js
@@ -20,66 +20,65 @@ let statsdUtils = require('@haventech/supertype').StatsdHelper;
 function processPost(req, res, sessions, controllers, nonObjTemplatelogLevel) {
     let processPostStartTime = process.hrtime();
 
-    let session = req.session;
-    let path = url.parse(req.originalUrl, true).query.path;
+	let session = req.session;
+	let path = url.parse(req.originalUrl, true).query.path;
 
-    establishServerSession(req, path, false, false, null, sessions, controllers, nonObjTemplatelogLevel, res)
-        .then(function ff(semotus) {
-            let ourObjectTemplate = semotus.objectTemplate;
-            let remoteSessionId = req.session.id;
+	establishServerSession(req, path, false, false, null, sessions, controllers, nonObjTemplatelogLevel, res)
+		.then(function ff(semotus) {
+			let ourObjectTemplate = semotus.objectTemplate;
+			let remoteSessionId = req.session.id;
 
-            if (typeof ourObjectTemplate.controller.processPost === 'function') {
-                Bluebird.resolve(ourObjectTemplate.controller.processPost(null, req.body, req, res))
-                    .then(function saveSessionAndEndResponse(controllerResp) {
+			if (typeof ourObjectTemplate.controller.processPost === 'function') {
+				Bluebird.resolve(ourObjectTemplate.controller.processPost(null, req.body, req, res))
+					.then(function saveSessionAndEndResponse(controllerResp) {
                         ourObjectTemplate.setSession(remoteSessionId);
-                        semotus.save(path, session, req);
-                        res.writeHead(
-                            controllerResp.status,
-                            controllerResp.headers || { 'Content-Type': 'text/plain' }
-                        );
-                        res.end(controllerResp.body);
+						semotus.save(path, session, req);
+						res.writeHead(
+							controllerResp.status,
+							controllerResp.headers || { 'Content-Type': 'text/plain' }
+						);
+						res.end(controllerResp.body);
 
-                        statsdUtils.computeTimingAndSend(
-                            processPostStartTime,
-                            'amorphic.webserver.process_post.response_time',
-                            { result: 'success' }
-                        );
-                    })
-                    .catch(function hh(e) {
-                        ourObjectTemplate.logger.info(
-                            {
-                                component: 'amorphic',
-                                module: 'processPost',
-                                activity: 'error'
-                            },
-                            'Error ' + e.message + e.stack
-                        );
+						statsdUtils.computeTimingAndSend(
+							processPostStartTime,
+							'amorphic.webserver.process_post.response_time',
+							{ result: 'success' }
+						);
+					})
+					.catch(function hh(e) {
+						ourObjectTemplate.logger.info(
+							{
+								component: 'amorphic',
+								module: 'processPost',
+								activity: 'error'
+							},
+							'Error ' + e.message + e.stack
+						);
 
-                        res.writeHead(500, { 'Content-Type': 'text/plain' });
-                        res.end('Internal Error');
+						res.writeHead(500, { 'Content-Type': 'text/plain' });
+						res.end('Internal Error');
 
-                        statsdUtils.computeTimingAndSend(
-                            processPostStartTime,
-                            'amorphic.webserver.process_post.response_time',
-                            { result: 'failure' }
-                        );
-                    });
-            }
-            else {
-                throw 'Not Accepting Posts';
-            }
-        })
-        .catch(function ii(error) {
-            logMessage('Error establishing session for processPost ', req.session.id, error.message + error.stack);
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end('Internal Error');
+						statsdUtils.computeTimingAndSend(
+							processPostStartTime,
+							'amorphic.webserver.process_post.response_time',
+							{ result: 'failure' }
+						);
+					});
+			} else {
+				throw 'Not Accepting Posts';
+			}
+		})
+		.catch(function ii(error) {
+			logMessage('Error establishing session for processPost ', req.session.id, error.message + error.stack);
+			res.writeHead(500, { 'Content-Type': 'text/plain' });
+			res.end('Internal Error');
 
-            statsdUtils.computeTimingAndSend(processPostStartTime, 'amorphic.webserver.process_post.response_time', {
-                result: 'failure'
-            });
-        });
+			statsdUtils.computeTimingAndSend(processPostStartTime, 'amorphic.webserver.process_post.response_time', {
+				result: 'failure'
+			});
+		});
 }
 
 module.exports = {
-    processPost: processPost
+	processPost: processPost
 };
