3b3724a993cd6e28575b0082edeff040094f2c49
Richard Yu <richard@haventech.us>
2022-06-23 14:24:44 -0400

    update logger for contracts

--- components/amorphic-contracts/HISTORY.md
+++ components/amorphic-contracts/HISTORY.md
@@ -1,3 +1,5 @@
+## 0.3.0
+* Add Logger object types and update LoggerFunction to use them
 ## 0.2.0
 * Changed type definition up to account for build function types
 ## 0.1.0
--- components/amorphic-contracts/index.d.ts
+++ components/amorphic-contracts/index.d.ts
@@ -29,6 +29,102 @@ export interface StatsdClientInterface {
 }
 
 /**
+ * Logger Object types and interfaces
+ */
+ export namespace Logger {
+    export type Data = {
+        [key: string]: any;
+    }
+
+    export type strNumKeyValuePair = {
+        [key: string]: number | string;
+    }
+
+    export interface ErrorData {
+        isHumanRelated: boolean;
+        code?: string;
+        error?: any;
+        message?: string;
+        data?: Data & { fromError?: never };
+    }
+
+    export interface Context {
+        globalId?: string,
+        globalSource?: string,
+        referenceId?: string,
+        referenceType?: string,
+        sessionId? : string,
+        piiLevel?: any,
+        data?: Data
+    }
+
+    export interface ErrorLog {
+        isHumanRelated?: boolean,
+        code?: string,
+        name?: string,
+        message?: string,
+        stack?: string,
+        data?: Data
+    }
+
+    interface GeoIP  {
+        latitude?: number,
+        longitude?: number,
+        cityName?: string,
+        stateName?: string,
+        postalCode?: string,
+        timeZone?: string,
+        countryName?: string,
+        countryCode?: string,
+        continentCode?: string
+    }
+
+    export interface Request {
+        url?: string,
+        method?: string,
+        clientIpAddress?: string,
+        data?: Data,
+        remoteUser?: string,
+        remoteAddress?: string,
+        line?: string,
+        length?: number,
+        time?: number,
+        headers?: string,
+        status?: number,
+        bytesSent?: number,
+        bodyBytesSent?: number,
+        httpReferer?: string,
+        httpUserAgent?: string,
+        httpXForwardedFor?: string,
+        upstreamAddress?: string,
+        upstreamStatus?: number,
+        upstreamResponseTime?: number,
+        upstreamConnectTime?: number,
+        upstreamHeaderTime?: number,
+        upstreamBytesReceived?: number,
+        upstreamBytesSent?: number,
+        geoip?: GeoIP
+    }
+
+    export interface Response {
+        status?: number,
+        data?: Data
+    }
+    
+    export interface Log {
+        module?: string,
+        function?: string,
+        category?: 'security' | 'availability' | 'request' | 'milestone'
+        message?: string,
+        context?: Context,
+        error?: ErrorLog,
+        request?: Request,
+        response?: Response,
+        data?: Data
+    }    
+}
+
+/**
  * logger function
  */
-export type LoggerFunction = (logLevel: string, logObject: any, ...rawLogData) => void
+export type LoggerFunction = (logLevel: string, logObject: Logger.Log, ...rawLogData) => void
