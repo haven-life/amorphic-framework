24abd19c69575d17a97f587710f24078937c55d3
Kamran <kamran@haventech.us>
2022-07-21 10:24:08 -0400

    bump packages

--- components/amorphic/typescript_tests/apps/common/js/ticketItem.ts
+++ components/amorphic/typescript_tests/apps/common/js/ticketItem.ts
@@ -1,10 +1,10 @@
-import {Supertype, supertypeClass, property, Remoteable, Persistable}  from '../../../../dist/index.js';
+import {Supertype, supertypeClass, property, Remoteable, Persistable, Bindable}  from '../../../../dist/index.js';
 import {Person} from './person';
 import {Ticket} from './ticket';
 console.log("Compiling TicketItem");
 
 @supertypeClass
-export class TicketItem extends Persistable(Remoteable(Supertype)) {
+export class TicketItem extends Persistable(Remoteable(Bindable((Supertype)))) {
 
     // Secure properties can only be set on the server
     @property({getType: ()=>{return Person}})
