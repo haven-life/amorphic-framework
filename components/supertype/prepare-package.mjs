30929f19a947e47596b9bacfa2fd1c417385884d
Richard Yu <richard@haventech.us>
2023-05-14 21:30:29 -0400

    redo tabbing

--- components/supertype/prepare-package.mjs
+++ components/supertype/prepare-package.mjs
@@ -3,28 +3,28 @@ import path from 'path';
 
 const buildDir = './dist';
 function createEsmModulePackageJson() {
-	console.log('Running prepare packages for ESM');
-	fs.readdir(buildDir, function (err, dirs) {
-		if (err) {
-			throw err;
-		}
-		dirs.forEach(function (dir) {
-			if (dir === 'esm') {
-				var packageJsonFile = path.join(buildDir, dir, '/package.json');
-				if (!fs.existsSync(packageJsonFile)) {
-					fs.writeFile(
-						packageJsonFile,
-						new Uint8Array(Buffer.from('{"type": "module"}')),
-						function (err) {
-							if (err) {
-								throw err;
-							}
-						}
-					);
-				}
-			}
-		});
-	});
+    console.log('Running prepare packages for ESM');
+    fs.readdir(buildDir, function (err, dirs) {
+        if (err) {
+            throw err;
+        }
+        dirs.forEach(function (dir) {
+            if (dir === 'esm') {
+                var packageJsonFile = path.join(buildDir, dir, '/package.json');
+                if (!fs.existsSync(packageJsonFile)) {
+                    fs.writeFile(
+                        packageJsonFile,
+                        new Uint8Array(Buffer.from('{"type": "module"}')),
+                        function (err) {
+                            if (err) {
+                                throw err;
+                            }
+                        }
+                    );
+                }
+            }
+        });
+    });
 }
 
 createEsmModulePackageJson();
