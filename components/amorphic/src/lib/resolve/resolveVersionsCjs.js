d38abb622c34d82884cd2da0def3f0f3d4055619
Richard Yu <richard@haventech.us>
2023-07-14 19:18:00 -0400

    fix indentation

--- components/amorphic/src/lib/resolve/resolveVersionsCjs.js
+++ components/amorphic/src/lib/resolve/resolveVersionsCjs.js
@@ -1,17 +1,17 @@
 async function resolveVersions(packageVersions) {
-	const readPackagePkg = await import('read-pkg');
-	const pkg = await readPackagePkg.readPackage();
-	const dependencies = pkg.dependencies;
+    const readPackagePkg = await import('read-pkg');
+    const pkg = await readPackagePkg.readPackage();
+    const dependencies = pkg.dependencies;
 
-	const setPackage = (packageVersions, dependencies, name) => {
-		packageVersions[name] = dependencies[name];
-	}
+    const setPackage = (packageVersions, dependencies, name) => {
+        packageVersions[name] = dependencies[name];
+    }
 
-	setPackage(packageVersions, dependencies, '@haventech/semotus');
-	setPackage(packageVersions, dependencies, '@haventech/supertype');
-	setPackage(packageVersions, dependencies, '@haventech/persistor');
-	setPackage(packageVersions, dependencies, '@haventech/bindster');
-	packageVersions['amorphic'] = pkg.version;
+    setPackage(packageVersions, dependencies, '@haventech/semotus');
+    setPackage(packageVersions, dependencies, '@haventech/supertype');
+    setPackage(packageVersions, dependencies, '@haventech/persistor');
+    setPackage(packageVersions, dependencies, '@haventech/bindster');
+    packageVersions['amorphic'] = pkg.version;
     return packageVersions;
 }
 
--- components/amorphic/src/lib/resolve/resolveVersionsEsm.js
+++ components/amorphic/src/lib/resolve/resolveVersionsEsm.js
@@ -1,17 +1,17 @@
 import { readPackage } from "read-pkg";
 
 export async function resolveVersions(packages) {
-	const pkg = await readPackage();
-	const dependencies = pkg.dependencies;
+    const pkg = await readPackage();
+    const dependencies = pkg.dependencies;
 
-	const setPackage = (packageVersions, dependencies, name) => {
-		packageVersions[name] = dependencies[name];
-	}
+    const setPackage = (packageVersions, dependencies, name) => {
+        packageVersions[name] = dependencies[name];
+    }
 
-	setPackage(packageVersions, dependencies, '@haventech/semotus');
-	setPackage(packageVersions, dependencies, '@haventech/supertype');
-	setPackage(packageVersions, dependencies, '@haventech/persistor');
-	setPackage(packageVersions, dependencies, '@haventech/bindster');
-	packageVersions['amorphic'] = pkg.version;
+    setPackage(packageVersions, dependencies, '@haventech/semotus');
+    setPackage(packageVersions, dependencies, '@haventech/supertype');
+    setPackage(packageVersions, dependencies, '@haventech/persistor');
+    setPackage(packageVersions, dependencies, '@haventech/bindster');
+    packageVersions['amorphic'] = pkg.version;
     return packageVersions;
 }
